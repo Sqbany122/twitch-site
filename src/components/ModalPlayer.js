5b6a32e134603761f2968d4ffa626b348141b186
Sqbany122 <oskar.lewandowski122@gmail.com>
2021-09-22 13:57:16 +0200

    added close player button to twitch clips player

--- src/components/ModalPlayer.js
+++ src/components/ModalPlayer.js
@@ -10,10 +10,13 @@ export default class ModalPlayer extends React.Component {
         return (
             <div className="modalPlayerBackground position-fixed fixed-top d-flex flex-column w-100 h-100 align-items-center justify-content-center" onClick={this.props.showPlayer}>
                 <div>
-                    <div className="w-100 modalButtons text-light my-2">
-                        <div className="downloadButton">
-                            <a href={this.props.downloadUrl} className="btn rounded-full px-4"><b>Pobierz klip (.mp4)</b></a>
-                        </div>
+                    <div className="w-100 d-flex justify-content-end text-light my-2">
+                        {this.props.downloadUrl ? (                
+                            <div className="downloadButton">
+                                <a href={this.props.downloadUrl} className="btn rounded-full px-4"><b>Pobierz klip (.mp4)</b></a>
+                            </div>
+                        ) : (null)}
+                        <button className="bg-white btn rounded-full margin-left" onClick={this.props.showPlayer}><i class="fas fa-times"></i></button>
                     </div>
                     <div className="modalPlayerBox">
                     <iframe
